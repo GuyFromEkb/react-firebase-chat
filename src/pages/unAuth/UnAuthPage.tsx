8ffedc31c9dc52c4db9f5ae071f34f0f061afb96
Sergey <graygmai1@gmail.com>
2022-10-29 13:22:44 +0500

    feat(unAuthPage): init

---
+++ src/pages/unAuth/UnAuthPage.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import FormRegister from "../../components/formRegister/FormRegister";
+import "./UnAuthPage.scss";
+
+const UnAuthPage: FC = () => {
+  return (
+    <>
+      <FormRegister />
+    </>
+  );
+};
+
+export default UnAuthPage;
