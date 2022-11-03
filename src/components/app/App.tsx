a622705ba7a3560605ea352e8c5aad2dd45d7dff
Sergey <graygmai1@gmail.com>
2022-11-04 03:41:03 +0500

    fix(ProtectedRout) fix Protectd Rout logic

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,23 +1,19 @@
-import React, { FC, useEffect } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { FC } from "react";
+import { Route, Routes } from "react-router-dom";
 import MainPage from "../../pages/main/MainPage";
 import LoginPage from "../../pages/login/LoginPage";
 import RegisterPage from "../../pages/register/RegisterPage";
 import "./App.scss";
 import { observer } from "mobx-react-lite";
-import { authStore } from "../../stores/authStore";
+
 import ProtectedRouteV2 from "../protectedRoute/ProtectedRouteV2";
 
 const App: FC = () => {
-  const { user, isLoading } = authStore;
-
-  if (isLoading) return <>LOADING</>;
-
   return (
     <>
       <Routes>
         <Route path="/">
-          <Route index element={<ProtectedRouteV2 user={user} outlet={<MainPage />} />} />
+          <Route index element={<ProtectedRouteV2 outlet={<MainPage />} />} />
           <Route index path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
         </Route>
--- src/components/protectedRoute/ProtectedRouteV2.tsx
+++ src/components/protectedRoute/ProtectedRouteV2.tsx
@@ -1,7 +1,33 @@
-import { User } from "firebase/auth";
 import { observer } from "mobx-react-lite";
 import { FC } from "react";
 import { Navigate } from "react-router-dom";
+import { authStore } from "../../stores/authStore";
+import Loader from "../loader/Loader";
+
+interface IProtectedRoute {
+  outlet: JSX.Element;
+}
+
+export const ProtectedRouteV2: FC<IProtectedRoute> = ({ outlet }) => {
+  const { user, isLoading } = authStore;
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (user) {
+    return outlet;
+  }
+
+  return <Navigate to="/login" />;
+};
+
+export default observer(ProtectedRouteV2);
+
+/* 
+###
+Old Version, from docs
+###
 
 interface IProtectedRoute {
   user: User | null;
@@ -15,5 +41,4 @@ export const ProtectedRouteV2: FC<IProtectedRoute> = ({ user, outlet }) => {
 
   return <Navigate to="/login" />;
 };
-
-export default observer(ProtectedRouteV2);
+*/
