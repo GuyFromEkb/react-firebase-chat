42b26298fb29be8b5044e9657f65f465153cf727
Sergey <graygmai1@gmail.com>
2022-11-04 03:38:27 +0500

    feat(Loader):init Loader

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import "./Loader.scss";
+
+const Loader: FC = () => {
+  return (
+    <div className="loader">
+      <svg viewBox="0 0 100 100">
+        <defs>
+          <filter id="loader-shadow">
+            <feDropShadow dx="0" dy="0" stdDeviation="1.5" floodColor="#ffffff7d" />
+          </filter>
+        </defs>
+        <circle className="loader__spinner" cx="50" cy="50" r="45" />
+      </svg>
+    </div>
+  );
+};
+
+export default Loader;
