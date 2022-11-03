2f50724e730810708c52dee942691740de53ff62
Sergey <graygmai1@gmail.com>
2022-11-04 02:05:54 +0500

    feat(RegisterPage): register page work with backend

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,38 +1,25 @@
-import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth";
-import { setDoc, doc, getFirestore } from "firebase/firestore";
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { toJS } from "mobx";
 import { FC, FormEvent, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import loadAvatar from "../../assets/img/addAvatar.png";
-import { auth, db, storage } from "../../firebase";
+import { authStore } from "../../stores/authStore";
 import "./RegisterPage.scss";
 
-interface CustomElements extends HTMLFormControlsCollection {
-  name: HTMLInputElement;
-  email: HTMLInputElement;
-  password: HTMLInputElement;
-  avatar: HTMLInputElement;
-}
-
-interface CustomForm extends HTMLFormElement {
-  readonly elements: CustomElements;
-}
-
-interface IFormState {
-  name: "";
+export interface IFormData {
+  displayName: "";
   email: "";
   password: "";
   avatar: File | undefined;
 }
 const RegisterPage: FC = () => {
-  const [formState, setFormState] = useState<IFormState>({
-    name: "",
+  const navigate = useNavigate();
+  const [formState, setFormState] = useState<IFormData>({
+    displayName: "",
     email: "",
     password: "",
     avatar: undefined,
   });
   const [prevAvatar, setPrevAvatar] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleChangeAvatar = (e: React.ChangeEvent<HTMLInputElement>) => {
     const imgFile = e.target.files?.[0];
@@ -48,7 +35,7 @@ const RegisterPage: FC = () => {
     };
   };
 
-  const handleChange = (prop: keyof IFormState) => (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeForm = (prop: keyof IFormData) => (event: React.ChangeEvent<HTMLInputElement>) => {
     if (prop === "avatar") {
       setFormState({ ...formState, [prop]: event.target.files?.[0] });
       handleChangeAvatar(event);
@@ -61,66 +48,40 @@ const RegisterPage: FC = () => {
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const data = {
-      name: formState.name,
+    const registerData = {
+      displayName: formState.displayName,
       email: formState.email,
       password: formState.password,
       avatar: formState.avatar,
     };
-    setIsLoading(true);
 
-    await registerUser(data);
-
-    setIsLoading(false);
+    await authStore.registerUser(registerData);
+    navigate("/");
   };
 
-  const registerUser = async ({ email, password, name: displayName, avatar }: IFormState) => {
-    if (!avatar) return;
-
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-
-    const date = new Date().getTime();
-    const storageRef = ref(storage, `${displayName + date}`);
-
-    await uploadBytesResumable(storageRef, avatar);
-    const downloadURL = await getDownloadURL(storageRef);
-
-    await updateProfile(res.user, {
-      displayName,
-      photoURL: downloadURL,
-    });
-
-    await setDoc(doc(db, "users", res.user.uid), {
-      uid: res.user.uid,
-      displayName,
-      email,
-      photoURL: downloadURL,
-    });
-
-    await setDoc(doc(db, "userChats", res.user.uid), {});
-  };
-
-  const avatarImg = prevAvatar ? prevAvatar : loadAvatar;
   return (
     <div className="form">
       <div className="form__wrap">
         <div className="form__title">React Chat</div>
         <div className="form__subtitle">Register</div>
         <form onSubmit={onSubmit} className="form-reg">
-          <input onChange={handleChange("name")} name="name" type="text" placeholder="Display Name" />
-          <input onChange={handleChange("email")} name="email" type="text" placeholder="Email" />
-          <input onChange={handleChange("password")} name="password" type="Password" placeholder="Password" />
+          <input onChange={handleChangeForm("displayName")} name="displayName" type="text" placeholder="Display Name" />
+          <input onChange={handleChangeForm("email")} name="email" type="text" placeholder="Email" />
+          <input onChange={handleChangeForm("password")} name="password" type="Password" placeholder="Password" />
           <label className="form-reg__input-file">
-            <img style={{ borderRadius: prevAvatar && "50%" }} src={avatarImg} alt="load-avatar" />
+            <img
+              style={{ borderRadius: prevAvatar && "50%" }}
+              src={prevAvatar ? prevAvatar : loadAvatar}
+              alt="load-avatar"
+            />
             <span>Add an avatar</span>
-            <input name="avatar" onChange={handleChange("avatar")} accept="image/*" type="file" />
+            <input name="avatar" onChange={handleChangeForm("avatar")} accept="image/*" type="file" />
           </label>
           <button>Sign Up</button>
           <div className="form-reg__footer">
             You do have an account? <Link to="/login">Login</Link>
           </div>
         </form>
-        <h4>{isLoading && "Loading"}</h4>
       </div>
     </div>
   );
