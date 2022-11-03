eaf3132c5ef81e29394c8fd7012a467c547c8242
Sergey <graygmai1@gmail.com>
2022-11-04 03:38:52 +0500

    feat(formOverlay): init Form Overlay

---
+++ src/components/formOverlay/FormOverlay.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import Loader from "../loader/Loader";
+import "./FormOverlay.scss";
+
+const FormOverlay: FC = () => {
+  return (
+    <div className="form-overlay">
+      <Loader />
+    </div>
+  );
+};
+
+export default FormOverlay;
