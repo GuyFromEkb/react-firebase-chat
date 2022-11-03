e76b459d3fe88d1f3fcd2bf4c2ea6d444181fe83
Sergey <graygmai1@gmail.com>
2022-11-04 02:06:23 +0500

    feat(LoginPage): LoginPage work with backend

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,16 +1,42 @@
-import { FC } from "react";
-import { Link } from "react-router-dom";
+import { FC, FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { authStore } from "../../stores/authStore";
 import "./LoginPage.scss";
+export interface IFormDataLogin {
+  email: "";
+  password: "";
+}
 
 const LoginPage: FC = () => {
+  const navigate = useNavigate();
+  const [formState, setFormState] = useState<IFormDataLogin>({
+    email: "",
+    password: "",
+  });
+  const handleChangeForm = (prop: keyof IFormDataLogin) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState({ ...formState, [prop]: event.target.value });
+  };
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const loginData = {
+      email: formState.email,
+      password: formState.password,
+    };
+
+    await authStore.login(loginData);
+    navigate("/");
+  };
+
   return (
     <div className="form">
       <div className="form__wrap">
         <div className="form__title">React Chat</div>
         <div className="form__subtitle">Login</div>
-        <form className="form-reg">
-          <input type="text" placeholder="Email" />
-          <input type="Password" placeholder="Password" />
+        <form onSubmit={onSubmit} className="form-reg">
+          <input onChange={handleChangeForm("email")} value={formState.email} type="text" placeholder="Email" />
+          <input onChange={handleChangeForm("password")} value={formState.password} type="Password" placeholder="Password" />
           <button>Login</button>
           <div className="form-reg__footer">
             You do not have an account? <Link to="/register">Register</Link>
