d64f12f16cb04962d786322b07baac77dc7be2ef
Sergey <graygmai1@gmail.com>
2022-11-04 02:04:35 +0500

    feat(authStore): init auth Store

---
+++ src/stores/authStore.ts
@@ -0,0 +1,82 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  User,
+} from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { makeAutoObservable, runInAction } from "mobx";
+import { auth, storage, db } from "../firebase";
+import { IFormDataLogin } from "../pages/login/LoginPage";
+import { IFormData } from "../pages/register/RegisterPage";
+import { authObserver } from "../utils/firebase/authObserver";
+
+class AuthStore {
+  user: User | null = null;
+  isLoading = false;
+
+  constructor() {
+    makeAutoObservable(this);
+    this._checkAuth();
+  }
+
+  registerUser = async (userData: IFormData) => {
+    const { email, password, displayName, avatar } = userData;
+    if (!avatar) return;
+
+    const { user } = await createUserWithEmailAndPassword(auth, email, password);
+    runInAction(() => (this.user = user));
+
+    const date = new Date().getTime();
+    const storageRef = ref(storage, `${displayName + date}`);
+
+    await uploadBytesResumable(storageRef, avatar);
+    const avatarURL = await getDownloadURL(storageRef);
+
+    await updateProfile(user, {
+      displayName,
+      photoURL: avatarURL,
+    });
+
+    await setDoc(doc(db, "users", user.uid), {
+      uid: user.uid,
+      displayName,
+      email,
+      photoURL: avatarURL,
+    });
+
+    await setDoc(doc(db, "userChats", user.uid), {});
+  };
+
+  logOut = async () => {
+    await signOut(auth);
+
+    runInAction(() => {
+      this.user = null;
+    });
+  };
+
+  login = async (userData: IFormDataLogin) => {
+    const { user } = await signInWithEmailAndPassword(auth, userData.email, userData.password);
+
+    runInAction(() => {
+      this.user = user;
+    });
+  };
+
+  private _checkAuth = async () => {
+    this.isLoading = true;
+    const res = await authObserver(auth);
+
+    runInAction(() => {
+      this.user = res;
+      this.isLoading = false;
+    });
+  };
+}
+
+const authStore = new AuthStore();
+
+export { authStore };
---
+++ src/utils/firebase/authObserver.ts
@@ -0,0 +1,16 @@
+import { User, onAuthStateChanged, Auth } from "firebase/auth";
+/**
+ * Пришлось самому делать промис, на observerAuth от Firebase, т.к. это асинъронная ф-ция, но они почему то её сделали как синхронную..
+ *
+ * @param {Auth} auth getAuth() in firebase
+ * @return user (если авторизован) | null
+ */
+const authObserver = (auth: Auth) => {
+  return new Promise<User | null>((resolve, reject) => {
+    onAuthStateChanged(auth, (user) => {
+      resolve(user || null);
+    });
+  });
+};
+
+export { authObserver };
