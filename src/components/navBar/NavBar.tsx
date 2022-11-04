edfdafc8db9ef89fa9f309016645d75f1090aa77
Sergey <graygmai1@gmail.com>
2022-11-04 15:20:22 +0500

    feat(NavBar) data from store

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -3,17 +3,14 @@ import { authStore } from "../../stores/authStore"
 import "./NavBar.scss"
 
 const NavBar: FC = () => {
-  const { logOut } = authStore
+  const { logOut, user } = authStore
   return (
     <div className="navbar">
       <div className="navbar__logo">React Chat</div>
       <div className="navbar__right-side">
         <div className="navbar__profile-wrap">
-          <img
-            src="https://avatars.mds.yandex.net/i?id=6cefb0f4ad555f4d38d3f4fdb266c5d0f178df88-5441329-images-thumbs&n=13"
-            alt="avatar"
-          />
-          <div>John</div>
+          <img src={user?.photoURL ? user?.photoURL : ""} alt="avatar" />
+          <div>{user?.displayName}</div>
         </div>
         <button onClick={logOut} className="navbar__logout">
           Logout
