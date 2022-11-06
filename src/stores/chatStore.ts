65270304274f90ba1d147afc72ba80b145a35a64
Sergey_nasty <zaebalacs@gmail.com>
2022-11-07 00:27:08 +0500

    fix(ChatStore): add getter to chats

--- src/stores/chatStore.ts
+++ src/stores/chatStore.ts
@@ -28,6 +28,10 @@ export interface IUserDateInfo {
 export interface ICurrentUserChats {
   date: IUserDateInfo
   userInfo: IUserChatInfo
+  lastMessage: {
+    senderId: string
+    text: string
+  }
 }
 interface ICurrentChatInfo {
   id: string
@@ -43,7 +47,7 @@ interface IMessage {
 
 class ChatStore {
   isLoading = false
-  chats: [string, ICurrentUserChats][] = []
+  private _chats: [string, ICurrentUserChats][] = []
   currentChatInfo: ICurrentChatInfo = {} as ICurrentChatInfo
   currentChatMessages: IMessage[] = []
 
@@ -94,11 +98,11 @@ class ChatStore {
     const docRef = doc(db, "userChats", user.uid)
 
     return onSnapshot(docRef, (doc) => {
-      if (!doc.metadata.hasPendingWrites) {
-        const userChats = doc.data()
-        if (userChats) {
-          runInAction(() => (this.chats = Object.entries(userChats)))
-        }
+      // if (!doc.metadata.hasPendingWrites) {
+      const userChats = doc.data()
+      if (userChats) {
+        runInAction(() => (this._chats = Object.entries(userChats)))
+        // }
       }
     })
 
@@ -112,9 +116,9 @@ class ChatStore {
   postMessage = async (text: string, currentUser: User) => {
     if (!this.currentChatInfo.id) return
 
-    const docRef = doc(db, "chats", this.currentChatInfo.id)
+    const docRefChat = doc(db, "chats", this.currentChatInfo.id)
 
-    await updateDoc(docRef, {
+    await updateDoc(docRefChat, {
       messages: arrayUnion({
         id: uuid(),
         text,
@@ -122,6 +126,38 @@ class ChatStore {
         date: Timestamp.now(),
       }),
     })
+
+    const docRefCurrentUserInChatList = doc(db, "userChats", currentUser.uid)
+
+    await updateDoc(docRefCurrentUserInChatList, {
+      [this.currentChatInfo.id + ".date"]: Timestamp.now(),
+      [this.currentChatInfo.id + ".lastMessage"]: {
+        senderId: currentUser.uid,
+        text,
+      },
+    })
+
+    const docRefRecipientUserInChatList = doc(
+      db,
+      "userChats",
+      this.currentChatInfo.recipientUserInfo.uid
+    )
+
+    await updateDoc(docRefRecipientUserInChatList, {
+      [this.currentChatInfo.recipientUserInfo.uid + ".date"]: Timestamp.now(),
+      [this.currentChatInfo.recipientUserInfo.uid + ".lastMessage"]: {
+        senderId: currentUser.uid,
+        text,
+      },
+    })
+  }
+
+  get chats() {
+    console.log("Getter")
+    return this._chats.slice().sort((a, b) => b[1].date.seconds - a[1].date.seconds)
+
+    // console.log('this._chats',toJS(this._chats))
+    // return this._chats
   }
 }
 
