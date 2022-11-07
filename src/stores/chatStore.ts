d4b8c72d3cab64a9063aa442e7e8b0e51a5bfe56
Sergey <graygmai1@gmail.com>
2022-11-08 01:42:28 +0500

    fix(chatStore) remove all message logic (new-store)

--- src/stores/chatStore.ts
+++ src/stores/chatStore.ts
@@ -33,31 +33,25 @@ export interface ICurrentUserChats {
     text: string
   }
 }
-interface ICurrentChatInfo {
+export interface ICurrentChatInfo {
   id: string
   recipientUserInfo: IUserChatInfo
-}
-
-interface IMessage {
-  senderId: string
-  text: string
-  date: string
-  id: string
 }
 
 class ChatStore {
   isLoading = false
   private _chats: [string, ICurrentUserChats][] = []
-  currentChatInfo: ICurrentChatInfo = {} as ICurrentChatInfo
-  currentChatMessages: IMessage[] = []
+  currentChatInfo: ICurrentChatInfo | null = null
 
   constructor() {
     makeAutoObservable(this)
   }
 
   toggleCurrentChat = (chatId: string, recipientUserInfo: IUserChatInfo) => {
-    this.currentChatInfo.id = chatId
-    this.currentChatInfo.recipientUserInfo = recipientUserInfo
+    this.currentChatInfo = {
+      id: chatId,
+      recipientUserInfo: recipientUserInfo,
+    }
   }
 
   createChat = async (user: IUser, currentUser: User | null) => {
@@ -66,10 +60,8 @@ class ChatStore {
 
     const docRef = doc(db, "chats", combinedId)
     const docSnap = await getDoc(docRef)
-    const a = true
-    if (/* !docSnap.exists() */ a) {
-      console.log("zawel v if", docSnap.exists())
 
+    if (!docSnap.exists()) {
       await Promise.all([
         setDoc(doc(db, "chats", combinedId), {
           messages: [],
@@ -80,7 +72,8 @@ class ChatStore {
             displayName: user.displayName,
             photoURL: user.photoURL,
           },
-          [combinedId + ".date"]: serverTimestamp(),
+          [combinedId + ".date"]: Timestamp.now(),
+          // [combinedId + ".date"]: serverTimestamp(),
         }),
         updateDoc(doc(db, "userChats", user.uid), {
           [combinedId + ".userInfo"]: {
@@ -88,10 +81,12 @@ class ChatStore {
             displayName: currentUser?.displayName,
             photoURL: currentUser?.photoURL,
           },
-          [combinedId + ".date"]: serverTimestamp(),
+          [combinedId + ".date"]: Timestamp.now(),
         }),
       ])
     }
+
+    this.toggleCurrentChat(combinedId, user)
   }
 
   subToFecthUserChats = async (user: User) => {
@@ -113,51 +108,11 @@ class ChatStore {
     // }
   }
 
-  postMessage = async (text: string, currentUser: User) => {
-    if (!this.currentChatInfo.id) return
-
-    const docRefChat = doc(db, "chats", this.currentChatInfo.id)
-
-    await updateDoc(docRefChat, {
-      messages: arrayUnion({
-        id: uuid(),
-        text,
-        senderId: currentUser.uid,
-        date: Timestamp.now(),
-      }),
-    })
-
-    const docRefCurrentUserInChatList = doc(db, "userChats", currentUser.uid)
-
-    await updateDoc(docRefCurrentUserInChatList, {
-      [this.currentChatInfo.id + ".date"]: Timestamp.now(),
-      [this.currentChatInfo.id + ".lastMessage"]: {
-        senderId: currentUser.uid,
-        text,
-      },
-    })
-
-    const docRefRecipientUserInChatList = doc(
-      db,
-      "userChats",
-      this.currentChatInfo.recipientUserInfo.uid
-    )
-
-    await updateDoc(docRefRecipientUserInChatList, {
-      [this.currentChatInfo.recipientUserInfo.uid + ".date"]: Timestamp.now(),
-      [this.currentChatInfo.recipientUserInfo.uid + ".lastMessage"]: {
-        senderId: currentUser.uid,
-        text,
-      },
-    })
-  }
-
   get chats() {
-    console.log("Getter")
-    return this._chats.slice().sort((a, b) => b[1].date.seconds - a[1].date.seconds)
-
-    // console.log('this._chats',toJS(this._chats))
-    // return this._chats
+    return this._chats
+      .filter((chat) => chat[1].lastMessage)
+      .slice()
+      .sort((a, b) => b[1].date?.seconds - a[1].date?.seconds)
   }
 }
 
