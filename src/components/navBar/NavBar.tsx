79a25b955edaea88086d772d4af7ba6a6fd41e09
Sergey <graygmai1@gmail.com>
2022-11-09 00:40:58 +0500

    fix(NavBar): fix style, remove logo

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -6,16 +6,13 @@ const NavBar: FC = () => {
   const { logOut, user } = authStore
   return (
     <div className="navbar">
-      <div className="navbar__logo">React Chat</div>
-      <div className="navbar__right-side">
-        <div className="navbar__profile-wrap">
-          <img src={user?.photoURL ? user?.photoURL : ""} alt="avatar" />
-          <div>{user?.displayName}</div>
-        </div>
-        <button onClick={logOut} className="navbar__logout">
-          Logout
-        </button>
+      <div className="navbar__profile-wrap">
+        <img src={user?.photoURL ? user?.photoURL : ""} alt="avatar" />
+        <div>{user?.displayName}</div>
       </div>
+      <button onClick={logOut} className="navbar__logout">
+        Logout
+      </button>
     </div>
   )
 }
