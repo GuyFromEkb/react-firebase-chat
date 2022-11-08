f1a57e76d2a7bd72511c0857f0121fcee2a9c9ac
Sergey <graygmai1@gmail.com>
2022-11-09 00:39:36 +0500

    feat(UserList): in accordion

--- src/components/userList/UserList.tsx
+++ src/components/userList/UserList.tsx
@@ -3,14 +3,14 @@ import { FC, useEffect } from "react"
 import { authStore } from "../../stores/authStore"
 import { chatStore } from "../../stores/chatStore"
 import { IUser, usersStore } from "../../stores/usersStore"
+import Accordion from "../accordion/Accordion"
 import UserItem from "../userItem/UserItem"
 import "./UserList.scss"
-// ts!
 
 const UserList: FC = () => {
+  const { user: currentUser } = authStore
   const { users, setCurrentUser } = usersStore
   const { createChat } = chatStore
-  const { user: currentUser } = authStore
 
   const handleClick = (user: IUser) => () => {
     createChat(user, currentUser)
@@ -22,17 +22,18 @@ const UserList: FC = () => {
 
   return (
     <>
-      <h5>users:</h5>
-      <div className="user-list">
-        {users.map((user) => (
-          <UserItem
-            onCreateChat={handleClick(user)}
-            key={user.uid}
-            photoUrl={user.photoURL}
-            displayName={user.displayName}
-          />
-        ))}
-      </div>
+      <Accordion title="Show all users" isOpenTitle="Hide all users">
+        <div className="user-list">
+          {users.map((user) => (
+            <UserItem
+              onCreateChat={handleClick(user)}
+              key={user.uid}
+              photoUrl={user.photoURL}
+              displayName={user.displayName}
+            />
+          ))}
+        </div>
+      </Accordion>
     </>
   )
 }
