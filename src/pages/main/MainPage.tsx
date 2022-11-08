8e3e38319047fce3119b3dc34d1f16e3d3f4480c
Sergey <graygmai1@gmail.com>
2022-11-08 22:10:47 +0500

    fix(MainPage) remove empty react fragment

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,18 +1,17 @@
 import { FC } from "react"
 import Chat from "../../components/chat/Chat"
 import SideBar from "../../components/sideBar/SideBar"
+
 import "./MainPage.scss"
 
 const MainPage: FC = () => {
   return (
-    <>
-      <main className="main">
-        <div className="container">
-          <SideBar />
-          <Chat />
-        </div>
-      </main>
-    </>
+    <main className="main">
+      <div className="container">
+        <SideBar />
+        <Chat />
+      </div>
+    </main>
   )
 }
 
