ebc57a2f953934de5d9b2599d5ba029be9978f35
Sergey_nasty <zaebalacs@gmail.com>
2022-11-13 16:59:49 +0500

    fix(userList):fix style

--- src/components/userList/UserList.tsx
+++ src/components/userList/UserList.tsx
@@ -3,33 +3,31 @@ import { FC } from "react"
 import { IUser } from "../../stores/usersStore"
 import Accordion from "../accordion/Accordion"
 import UserItem from "../userItem/UserItem"
-import "./UserList.scss"
 import { useStore } from "../../hooks/useStore"
+import "./UserList.scss"
 
 const UserList: FC = () => {
   const { usersStore, chatStore } = useStore()
   const { users } = usersStore
-  const { createChat } = chatStore
+  const { createChat, currentChatInfo } = chatStore
 
   const handleClick = (user: IUser) => () => {
     createChat(user)
   }
 
   return (
-    <>
-      <Accordion title="Show all users" isOpenTitle="Hide all users">
-        <div className="user-list">
-          {users.map((user) => (
-            <UserItem
-              onCreateChat={handleClick(user)}
-              key={user.uid}
-              photoUrl={user.photoURL}
-              displayName={user.displayName}
-            />
-          ))}
-        </div>
-      </Accordion>
-    </>
+    <Accordion title="Show all users" isOpenTitle="Hide all users">
+      <div className="user-list">
+        {users.map((user) => (
+          <UserItem
+            onCreateChat={handleClick(user)}
+            key={user.uid}
+            photoUrl={user.photoURL}
+            displayName={user.displayName}
+          />
+        ))}
+      </div>
+    </Accordion>
   )
 }
 
