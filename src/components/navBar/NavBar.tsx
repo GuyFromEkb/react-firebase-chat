0201a96286858a5bed155b91917295514163dc74
Sergey <graygmai1@gmail.com>
2022-11-30 01:32:40 +0500

    feat(navBar): add modal

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,23 +1,33 @@
 import "./NavBar.scss"
 
 import { useStore } from "hooks/useStore"
-import { FC } from "react"
+import { FC, useState } from "react"
 
 import Avatar from "components/avatar/Avatar"
+import ModalProfileSetting from "components/modal/Modal"
 
 const NavBar: FC = () => {
   const { authStore } = useStore()
   const { logOut, user } = authStore
+  const [edditProfileModal, setEdditProfileModal] = useState(false)
   return (
-    <div className="navbar">
-      <div className="navbar__profile-wrap">
-        <Avatar className="navbar__avatar" photoUrl={user?.photoURL} />
-        <div>{user?.displayName}</div>
+    <>
+      <div className="navbar">
+        <div className="navbar__profile-wrap">
+          <Avatar className="navbar__avatar" photoUrl={user?.photoURL} />
+          <div>{user?.displayName}</div>
+        </div>
+        <button onClick={() => setEdditProfileModal(!edditProfileModal)}>test</button>
+        <button onClick={logOut} className="navbar__logout">
+          Logout
+        </button>
       </div>
-      <button onClick={logOut} className="navbar__logout">
-        Logout
-      </button>
-    </div>
+
+      <ModalProfileSetting
+        isShow={edditProfileModal}
+        onClose={() => setEdditProfileModal(!edditProfileModal)}
+      />
+    </>
   )
 }
 
